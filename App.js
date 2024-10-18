cec9d8d7fba91b2312a22aef9d6d03e0a907880b
Mukesh Singh <mukeshsingh@192.168.1.5>
2024-10-18 11:34:02 +0530

    episode_2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 let header = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1", key: "child1" }, [React.createElement("span", { style: { fontWeight: 500 }, id: "heading1", key: 'span1' }, "Hello World from ReactJS!!"), React.createElement("h2", { id: "heading1", key: 'h2_1' }, "I am h2 tag")]), React.createElement("div", { id: "child2", key: "child2" }, [React.createElement("span", { style: { fontWeight: 500 }, id: "heading2", key: 'span2' }, "Hello World from ReactJS!!"), React.createElement("h2", { id: "heading2", key: 'h2_2' }, "I am h2 tag")])]);
 
 // console.log(header); //object
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-📖 --> Namaste React Course <-- 📝
+# 📖 --> Namaste React Course <-- 📝
+
+# Parcel
+
+- Local Server
+- Dev Build
+- Hosting Dev Build to Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minify and Optimise our file
+- Bundle our file
+- Compress our file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - a support for older browsers
+- Diagnostic
+- Error Handling Suggestions
+- HTTPs support
+- Tree shaking - remove unused code
+- Different dev and prod bundles
